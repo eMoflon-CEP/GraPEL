3b89b3054843ade1d898b58c8987dc4030876ff7
Konrad <29203289+konivik@users.noreply.github.com>
2020-10-12 14:23:25 +0200

    Add get for model and find for a server via server ID

--- TestsuiteGrapeL/test/org/emoflon/grapel/testsuite/mnetwork/MNetworkAbstractTest.java
+++ TestsuiteGrapeL/test/org/emoflon/grapel/testsuite/mnetwork/MNetworkAbstractTest.java
@@ -1,9 +1,15 @@
 package org.emoflon.grapel.testsuite.mnetwork;
 
+import java.util.ArrayList;
+
 import org.emoflon.grapel.testsuite.GrapeLAppTestCase;
 
 import MNetworkGrapeL.grapel.Mnetwork.MnetworkGrapeLAPI;
 import MNetworkGrapeL.grapel.Mnetwork.MnetworkGrapeLHiPEEngine;
+import MNetworkGrapeL.grapel.Mnetwork.eventhandler.ContainerEventHandler;
+import mNetwork.MEMIK;
+import mNetwork.Network;
+import mNetwork.Server;
 
 public abstract class MNetworkAbstractTest extends GrapeLAppTestCase<MnetworkGrapeLAPI> {
 	MnetworkGrapeLAPI api;
@@ -16,5 +22,22 @@ public abstract class MNetworkAbstractTest extends GrapeLAppTestCase<MnetworkGra
 		MnetworkGrapeLAPI api = new MnetworkGrapeLHiPEEngine();
 		initAPI(api,modelInstanceFileName);
 		return api;
+	}
+	protected MEMIK getModel() {
+		ContainerEventHandler containerHandler = api.getContainerEventHandler();
+		ArrayList<MEMIK> models = new ArrayList<MEMIK>();
+
+		api.update();
+		containerHandler.getAllEvents().forEach(event -> models.add(event.getMemik()));
+		MEMIK model = models.get(0);
+		
+		return model;
+	}
+	protected Server findServer(Network network, int ID) {
+		for(Server s: network.getServer())
+			if(s.getId()==ID)
+				return s;
+		System.out.println("No matching server");
+		return null;
 	}
 }
