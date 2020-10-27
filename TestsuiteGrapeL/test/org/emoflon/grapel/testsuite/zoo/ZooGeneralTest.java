1c897501767bc204d9a1caa16fa51ffc2ddaa576
Konrad <29203289+konivik@users.noreply.github.com>
2020-10-27 20:32:18 +0100

    Update model, .grpl and ZooGeneralTest for name and age tests and enviroment checks

--- TestsuiteGrapeL/test/org/emoflon/grapel/testsuite/zoo/ZooGeneralTest.java
+++ TestsuiteGrapeL/test/org/emoflon/grapel/testsuite/zoo/ZooGeneralTest.java
@@ -6,7 +6,13 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import ZooGrapeL.grapel.Zoo.eventhandler.AnimalsWithSameAgeEventHandler;
+import ZooGrapeL.grapel.Zoo.eventhandler.AnimalsWithSameNameAndAge1EventHandler;
+import ZooGrapeL.grapel.Zoo.eventhandler.AnimalsWithSameNameAndAge2EventHandler;
+import ZooGrapeL.grapel.Zoo.eventhandler.AnimalsWithSameNameAndAge3EventHandler;
+import ZooGrapeL.grapel.Zoo.eventhandler.AnimalsWithSameNameEventHandler;
 import ZooGrapeL.grapel.Zoo.eventhandler.ContainerEventHandler;
+import ZooGrapeL.grapel.Zoo.eventhandler.EnviromentMissmatchWarningEventHandler;
 import ZooGrapeL.grapel.Zoo.eventhandler.HumidityWarningEventHandler;
 import ZooGrapeL.grapel.Zoo.eventhandler.TempartureWarningEventHandler;
 import zoo.Enclosure;
@@ -38,13 +44,13 @@ public class ZooGeneralTest extends ZooAbstractTest {
 			i = i + e.getAnimals().size();
 		assertEquals(12,i);
 	}
-//	@Test
-//	public void produces4EnviromentWarnings() {
-//		EnviromentMissmatchWarningEventHandler enviromentHandler = api.getEnviromentMissmatchWarningEventHandler();
-//		
-//		api.update();
-//		assertEquals(4, enviromentHandler.getAllEvents().size());
-//	}
+	@Test
+	public void produces4EnviromentWarnings() {
+		EnviromentMissmatchWarningEventHandler enviromentHandler = api.getEnviromentMissmatchWarningEventHandler();
+		
+		api.update();
+		assertEquals(4, enviromentHandler.getAllEvents().size());
+	}
 	@Test
 	public void produces4TemperatureWarnings() {
 		TempartureWarningEventHandler temperatureHandler = api.getTempartureWarningEventHandler();
@@ -59,11 +65,24 @@ public class ZooGeneralTest extends ZooAbstractTest {
 		api.update();
 		assertEquals(5, humidityHandler.getAllEvents().size()); // needs to change, if mixed is ignored
 	}
-//	@Test
-//	public void produces1SameAgeAndNameWarning() {
-//		AnimalsWithSameNameAndAge1EventHandler sameNameAndAge1 = api.getAnimalsWithSameNameAndAge1EventHandler();
-//		
-//	}
+	@Test
+	public void producesSameAgeAndNameWarning() {
+		AnimalsWithSameNameAndAge1EventHandler sameNameAndAge1 = api.getAnimalsWithSameNameAndAge1EventHandler();
+		AnimalsWithSameNameAndAge2EventHandler sameNameAndAge2 = api.getAnimalsWithSameNameAndAge2EventHandler();
+		AnimalsWithSameNameAndAge3EventHandler sameNameAndAge3 = api.getAnimalsWithSameNameAndAge3EventHandler();
+		
+		AnimalsWithSameNameEventHandler sameName = api.getAnimalsWithSameNameEventHandler();
+		AnimalsWithSameAgeEventHandler sameAge = api.getAnimalsWithSameAgeEventHandler();
+		
+		api.update();
+		assertEquals(4, sameName.getAllEvents().size());
+		assertEquals(4, sameAge.getAllEvents().size());
+		
+		
+		assertEquals(2, sameNameAndAge1.getAllEvents().size());
+		assertEquals(4, sameNameAndAge2.getAllEvents().size()); // double because doubled name and Events
+		assertEquals(2, sameNameAndAge3.getAllEvents().size());
+	}
 	
 	@After
 	public void dispose() {
