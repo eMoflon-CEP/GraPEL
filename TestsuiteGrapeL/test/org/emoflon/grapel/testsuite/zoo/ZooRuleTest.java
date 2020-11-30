7fdd781203c36535534370ca546de0088f8d1f66
Konrad <29203289+konivik@users.noreply.github.com>
2020-11-30 18:24:55 +0100

    Add rule tests for zoo, which not always work as expected

---
+++ TestsuiteGrapeL/test/org/emoflon/grapel/testsuite/zoo/ZooRuleTest.java
@@ -0,0 +1,70 @@
+package org.emoflon.grapel.testsuite.zoo;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedList;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import ZooGrapeL.grapel.Zoo.eventhandler.ApeKleeWarningEventHandler;
+import ZooGrapeL.grapel.Zoo.eventhandler.MoreAnimalsThanCapacityEventHandler;
+import ZooGrapeL.grapel.Zoo.eventhandler.MoveAnimalToEnclousureWithFreeSpaceEventHandler;
+import ZooGrapeL.grapel.Zoo.eventhandler.RenameApeEventHandler;
+import zoo.Animal;
+import zoo.Ape;
+
+public class ZooRuleTest extends ZooAbstractTest {
+	@Before
+	public void start() {
+		api = this.init("test1.xmi");
+	}
+	@Test
+	public void ruleAppliesNewNameToApeKlee() {
+		ApeKleeWarningEventHandler kleeWarningEventHandler = api.getApeKleeWarningEventHandler();
+		RenameApeEventHandler renameApeEventHandler = api.getRenameApeEventHandler();
+		
+		api.setRuleAutoApply(true);
+		renameApeEventHandler.applyAutmatically();
+		assertEquals(0, kleeWarningEventHandler.getAllEvents().size());
+		assertEquals(0, renameApeEventHandler.getAllEvents().size());
+		
+		api.update();
+		assertEquals(1, kleeWarningEventHandler.getAllEvents().size());
+		assertEquals(1, renameApeEventHandler.getAllEvents().size());
+		
+		LinkedList<Ape> apes = new LinkedList<Ape>();
+		kleeWarningEventHandler.getAllEvents().forEach(event -> apes.add(event.getAnimal()));
+		Ape klee = apes.get(0);
+		
+		assertEquals(true, klee.getName().equals("Richter"));
+	}
+	@Test
+	public void ruleAppliesOnOverfilledEnclosure() {
+		MoveAnimalToEnclousureWithFreeSpaceEventHandler moveHandler = api.getMoveAnimalToEnclousureWithFreeSpaceEventHandler();
+		MoreAnimalsThanCapacityEventHandler capacityHandler = api.getMoreAnimalsThanCapacityEventHandler();
+		
+		api.setRuleAutoApply(true);
+		moveHandler.applyAutmatically();
+		assertEquals(0, moveHandler.getAllEvents().size());
+		
+		api.update();
+		assertEquals(1, capacityHandler.getAllEvents().size());
+		assertEquals(1, moveHandler.getAllEvents().size());
+		LinkedList<Animal> animals = new LinkedList<Animal>();
+		moveHandler.getAllEvents().forEach(event -> animals.add(event.getAnimal()));
+		animals.forEach(a -> System.out.println(a.getName()));
+	}
+	
+	
+	@After
+	public void dispose() {
+		api.dispose();
+		try {
+			Thread.sleep(400);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
