dd0393879d54c8f0ad23c0a83fd1d123c655fe72
Konrad <29203289+konivik@users.noreply.github.com>
2021-08-31 14:24:58 +0200

    Add javadoc for the utils package

--- grapel.builder/src/org/emoflon/cep/util/IOUtils.java
+++ grapel.builder/src/org/emoflon/cep/util/IOUtils.java
@@ -4,7 +4,16 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
+/**
+ * Utilities for interacting with file IO
+ */
 public final class IOUtils {
+	/**
+	 * Gets the file at the given path
+	 * @param path to the file
+	 * @return the file at the location given by the path
+	 * @throws IOException if no file is present at the given path
+	 */
 	public static File getFile(String path) throws IOException {
 		File file = new File(path);
 		if(!file.exists())
@@ -13,6 +22,12 @@ public final class IOUtils {
 		return file;
 	}
 	
+	/**
+	 * Loads a text file at a given path
+	 * @param path to the text file
+	 * @return a string with the contents of the file.
+	 * @throws IOException if no file is present at the location given by the path
+	 */
 	public static String loadTextFile(String path) throws IOException {
 		File file = getFile(path);
 		String content = new String(Files.readAllBytes(file.toPath()));
--- grapel.builder/src/org/emoflon/cep/util/ProcessRunner.java
+++ grapel.builder/src/org/emoflon/cep/util/ProcessRunner.java
@@ -4,16 +4,40 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * Utilities for creating and stopping a process via command
+ */
 public class ProcessRunner extends Thread{
+	/**
+	 * Process which the runner should control
+	 */
 	private Process pr = null;
+	/**
+	 * The process execution arguments
+	 */
 	private final String[] execArgs;
+	/**
+	 * If the process is currently running
+	 */
 	private boolean running = false;
+	/**
+	 * If debug mode is enabled
+	 */
 	private boolean debug = false;
 	
+	/**
+	 * Constructor for a process runner for given exec arguments
+	 * @param execArgs as executable arguments to run the Process
+	 */
 	public ProcessRunner(final String[] execArgs) {
 		this.execArgs = execArgs;
 	}
 	
+	/**
+	 * Constructor for a process runner for given exec arguments and debug mode
+	 * @param execArgs as executable arguments to run the Process
+	 * @param debug if the debug mode should be enabled
+	 */
 	public ProcessRunner(final String[] execArgs, boolean debug) {
 		this.execArgs = execArgs;
 		this.debug = debug;
@@ -56,27 +80,34 @@ public class ProcessRunner extends Thread{
 		}
 	}
 	
+	/**
+	 * Sets the debug parameter
+	 * @param debug parameter for enabling/disabling debug
+	 */
 	public synchronized void setDebug(boolean debug) {
 		this.debug = debug;
 	}
 	
+	/**
+	 * Closes the process and updates the running state
+	 */
 	public synchronized void close() {
+		// if process is already terminated set running to false and exit
 		if(!pr.isAlive()) {
 			running = false;
 			return;
 		}
-//		System.out.println("Killing: "+pr.toString()+" status: "+pr.isAlive());
 		
+		// terminate process and wait until process has died
 		pr.destroy();
 		while(pr.isAlive()) {
 			try {
 				Thread.sleep(10);
 			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-//		System.out.println("Exited with: "+pr.exitValue());
+		
 		running = false;
 	}
 }
